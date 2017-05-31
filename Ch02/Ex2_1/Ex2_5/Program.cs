06c6d2d0ba0ee72cbd7b6f12d4c4af2099c12095
neighborpil <neighborpil@naver.com>
2017-06-01 01:16:50 +0900

    2_5 finish

--- Ch02/Ex2_1/Ex2_5/Program.cs
+++ Ch02/Ex2_1/Ex2_5/Program.cs
@@ -16,6 +16,34 @@ namespace Ex2_5
             WriteTotalMemory("배열확보전");
             WriteLine($"배열의 요소 수:{numbers.Length}\n");
 
+            //종래의 방식
+            {
+                double ave = 0;
+                int count = 0;
+                foreach(int n in numbers)
+                {
+                    if(n % 2 == 0)
+                    {
+                        count++;
+                        ave += (n - ave) / count;
+                    }
+                }
+                WriteTotalMemory("합계전");
+                WriteLine($"짝수만의 평균 : {ave}\n");
+            }
+
+            //Linq
+            {
+                //偶数だけを取り出したコレクションを得る
+                var evenNumbers = numbers.Where(n => n % 2 == 0);
+                WriteTotalMemory("偶数だけ取り出した後"); //
+
+                //平均値を求める
+                var ave = evenNumbers.Average();
+                WriteTotalMemory("計算後(Linq)");
+                WriteLine($"偶数だけの平均値(Linq) : {ave}");
+            }
+
 #if DEBUG
             ReadKey();
 #endif

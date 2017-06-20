be5aa45d33313af44d278fac4051689bd8dfef9b
Company@DESKTOP-M4SNJ2V <feelongpark@samwoosoft.com>
2017-06-20 12:46:29 +0900

    5_2 finished

---
+++ CH05/CH05_Examples/Ex5_1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ex5_1
+{
+    public class Sample
+    {
+        public string Kind { get; set; }
+        public int Value { get; set; }
+    }
+
+    public static class SampleExtensions
+    {
+        public static int SumValues(this IEnumerable<Sample> samples)
+        {
+            WriteLine("※SumValues開始");
+
+            int sum = 0;
+            foreach (var s in samples)
+            {
+                sum += s.Value;
+                WriteLine($"[3] Sum : {s.Kind}, {s.Value}, <sum={sum}>");
+            }
+            WriteLine("※SumValues終了");
+
+            return sum;
+        }
+    }
+
+    class Program
+    {
+        static Sample CreateFromCsvLine(string line)
+        {
+            WriteLine();
+            WriteLine($"[1] Select : {line}");
+
+            // カンマで分解する
+            string[] items = line.Split(',');
+
+            // Sampleオブジェクトを作って返す
+            return new Sample()
+            {
+                Kind = items[0].Trim(),
+                Value = int.Parse(items[1].Trim())
+            };
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}

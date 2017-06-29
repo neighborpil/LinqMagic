3cc5a9f11e01206b28f903ef49c1f5efac6f1f84
Company@DESKTOP-M4SNJ2V <feelongpark@samwoosoft.com>
2017-06-29 13:04:59 +0900

    ch09 ex update

---
+++ CH09/CH09Exs/List9_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace List9_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Enumerable.Range(1, 10).ToList();
+            //LINQ 拡張メソッド
+            var results = numbers.Where(n => n % 2 == 0);
+            WriteNumbers(results, "偶数だけ(LINQ)");
+
+            WriteLine();
+
+            //クエリ式
+            var resultsQ = from n in numbers
+                          where n % 2 == 0
+                          select n;
+            WriteNumbers(resultsQ, "(クエリ式)");
+
+        }
+
+        private static void WriteNumbers(IEnumerable<int> results, string str)
+        {
+            Write($"results{str} : ");
+            foreach (var item in results)
+            {
+                Write($" {item}");
+            }
+
+#if DEBUG
+            ReadKey();
+#endif
+        }
+    }
+}
---
+++ CH09/CH09Exs/List9_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List9_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // LINQ　拡張メソッド(5.6節のコードを改変
+            int sum = File.ReadLines(@"\sample.csv").
+                Select(line => CreateFromCSVLine(line))
+                .Where(sample => IsKindA(sample))
+                .Sum(s => s.Value);
+
+            //クエリ式
+            int sum = (
+                from line in File.ReadLines(@"\sample.csv")
+                select CreateFromCSVLine(line) into sample
+                where IsKindA(sample)
+                select sample)
+                .Sum(s => s.Value);
+
+        }
+
+        private static bool IsKindA(object sample)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static object CreateFromCSVLine(string line)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

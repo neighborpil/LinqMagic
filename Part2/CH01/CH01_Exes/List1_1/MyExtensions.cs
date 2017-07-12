1de8407bf5d9e2baec8a69780f199abf24f450c0
Company@DESKTOP-M4SNJ2V <feelongpark@samwoosoft.com>
2017-07-12 20:07:04 +0900

    part 2 ch2 making

---
+++ Part2/CH01/CH01_Exes/List1_1/MyExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtension
+{
+    public static class MyExtensions
+    {
+        /// <summary>
+        /// 문자열 Reverse
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string Reverse(this string s)
+        {
+            char[] c = s.ToCharArray(); // string을 char의 배열로 뽑아 낸다
+            Array.Reverse(c);
+            return new string(c);    //임시 구현
+        }
+
+        public static string Head(this string s, int maxLength)
+        {
+            if (s == null)
+                return s;
+            if (s.Length <= maxLength)
+                return s;
+            return s.Substring(0, maxLength);
+        }
+    }
+}
---
+++ Part2/CH01/CH01_Exes/List1_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringExtension;
+
+namespace List1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "1 2 3 4 5";
+
+            //확장 메소드의 일반적 호출 방법
+            string s2 = s1.Reverse();
+            Console.WriteLine(s2);
+            //출력
+            // 5 4 3 2 1
+
+            string s3 = StringExtension.MyExtensions.Reverse(s1);
+            Console.WriteLine(s3);
+
+            string s4 = "ABC".Reverse().ToLower();
+            Console.WriteLine(s4);
+
+            Console.WriteLine("12345".Head(3));
+            //출력
+            //123
+#if DEBUG
+            Console.ReadKey();
+#endif
+
+        }
+    }
+}
